23cd3cb98c06607742af26f6b6366247f6d0943e
gkoncu <gkoncu0@gmail.com>
2025-10-31 20:38:29 +0300

    Create TenantService.cs

---
+++ src/XYZ.Infrastructure/MultiTenancy/TenantService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Infrastructure.MultiTenancy
+{
+    public interface ITenantService
+    {
+        int GetCurrentTenantId();
+    }
+
+    public class TenantService : ITenantService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public TenantService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public int GetCurrentTenantId()
+        {
+            // TODO: Get from subdomain/claim
+            return 1;
+        }
+    }
+}
