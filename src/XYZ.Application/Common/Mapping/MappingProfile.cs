a59d4447ddd99cbc25df28ef7f0e555b6bf8981d
gkoncu <gkoncu0@gmail.com>
2025-11-05 18:31:25 +0300

    Update MappingProfile.cs

--- src/XYZ.Application/Common/Mapping/MappingProfile.cs
+++ src/XYZ.Application/Common/Mapping/MappingProfile.cs
@@ -13,13 +13,19 @@ namespace XYZ.Application.Common.Mapping
     {
         public MappingProfile()
         {
-            // Student mappings
             CreateMap<Student, StudentDto>()
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.User.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.User.LastName))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.User.Email))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.User.PhoneNumber))
                 .ForMember(dest => dest.ClassName, opt => opt.MapFrom(src => src.Class != null ? src.Class.Name : null))
-                .ForMember(dest => dest.CoachName, opt => opt.MapFrom(src =>
-                    src.Class.Coach != null ? src.Class.Coach.User.FullName : string.Empty));
+                .ForMember(dest => dest.CoachNames, opt => opt.MapFrom(src =>
+                    src.Class != null ?
+                    src.Class.Coaches.Select(c => $"{c.User.FirstName} {c.User.LastName}").ToList() :
+                    new List<string>()));
 
             CreateMap<CreateStudentDto, Student>();
+
             CreateMap<UpdateStudentDto, Student>();
         }
     }
