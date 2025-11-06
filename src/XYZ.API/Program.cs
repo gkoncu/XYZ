d1673bbe7b4f3f130bf1b5587a2ebe88568b1d0a
gkoncu <gkoncu0@gmail.com>
2025-11-06 16:59:38 +0300

    Update Program.cs

--- src/XYZ.API/Program.cs
+++ src/XYZ.API/Program.cs
@@ -1,22 +1,16 @@
 using XYZ.Application.Common.Interfaces;
 using XYZ.Application.Services;
-using XYZ.Infrastructure;
-using XYZ.Infrastructure.Data;
-using XYZ.Infrastructure.ExternalServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddApplication();
-builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IDataScopeService, DataScopeService>();
-builder.Services.AddScoped<ITenantService, TenantService>();
-builder.Services.AddScoped<IFileService, FileService>();
-
 builder.Services.AddScoped<IApplicationDbContext>(provider =>
     provider.GetRequiredService<ApplicationDbContext>());
 
