357487525e913ed28179b7842a537ac1663ed28d
gkoncu <gkoncu0@gmail.com>
2025-11-06 19:49:09 +0300

    Config Mapster

---
+++ src/XYZ.Application/Common/Mappings/GlobalMappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using Mapster;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XYZ.Application.Common.Interfaces;
+using XYZ.Domain.Entities;
+
+namespace XYZ.Application.Common.Mappings
+{
+    public class GlobalMappingConfig : IMapsterConfig
+    {
+        public void Configure(TypeAdapterConfig config)
+        {
+            config.NewConfig<Student, StudentDto>()
+                .Map(dest => dest.FullName, src => $"{src.FirstName} {src.LastName}")
+                .Map(dest => dest.Age, src => DateTime.Now.Year - src.BirthDate.Year);
+        }
+    }
+}
