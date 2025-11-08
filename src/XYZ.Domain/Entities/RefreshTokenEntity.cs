a1b09d6d932d1de21cd94aaf21f60df3a74a6825
gkoncu <gkoncu0@gmail.com>
2025-11-08 22:20:52 +0300

    Create RefreshTokenEntity.cs

---
+++ src/XYZ.Domain/Entities/RefreshTokenEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Domain.Entities
+{
+    public sealed class RefreshToken
+    {
+        public Guid Id { get; set; }
+
+        public string UserId { get; set; } = null!;
+
+        public string Hash { get; set; } = null!;
+
+        public int? TenantId { get; set; }
+
+        public DateTimeOffset CreatedAtUtc { get; set; }
+        public DateTimeOffset ExpiresAtUtc { get; set; }
+        public DateTimeOffset? RevokedAtUtc { get; set; }
+        public Guid? ReplacedByTokenId { get; set; }
+        public string? CreatedByIp { get; set; }
+        public string? UserAgent { get; set; }
+    }
+}
+
