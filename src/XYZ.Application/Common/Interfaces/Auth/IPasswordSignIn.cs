e04028d3caa5d0c832701153756beaa43c7406bb
gkoncu <gkoncu0@gmail.com>
2025-11-08 14:17:16 +0300

    Create Auth Interfaces Jwt Settings

---
+++ src/XYZ.Application/Common/Interfaces/Auth/IPasswordSignIn.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XYZ.Domain.Enums;
+
+namespace XYZ.Application.Common.Interfaces.Auth
+{
+    public interface IPasswordSignIn
+    {
+        Task<PasswordSignInResult> CheckAsync(
+            string userId,
+            string password,
+            CancellationToken ct = default);
+    }
+
+    public sealed record PasswordSignInResult(
+        PasswordSignInStatus Status
+    )
+    {
+        public bool Succeeded => Status == PasswordSignInStatus.Success;
+    }
+}
