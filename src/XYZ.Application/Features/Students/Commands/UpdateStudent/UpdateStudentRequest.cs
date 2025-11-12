5688cd0a2e7ae6a79cf4b9befd994d7d740553bc
gkoncu <gkoncu0@gmail.com>
2025-11-12 23:07:21 +0300

    UpdateStudent Query

---
+++ src/XYZ.Application/Features/Students/Commands/UpdateStudent/UpdateStudentRequest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Application.Features.Students.Commands.UpdateStudent
+{
+    public class UpdateStudentRequest
+    {
+        public int StudentId { get; set; }
+
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; } = null!;
+        public string BloodType { get; set; } = null!;
+        public string? IdentityNumber { get; set; }
+        public string? Address { get; set; }
+
+        public string? Parent1FirstName { get; set; }
+        public string? Parent1LastName { get; set; }
+        public string? Parent1Email { get; set; }
+        public string? Parent1PhoneNumber { get; set; }
+
+        public string? Parent2FirstName { get; set; }
+        public string? Parent2LastName { get; set; }
+        public string? Parent2Email { get; set; }
+        public string? Parent2PhoneNumber { get; set; }
+
+        public string? Notes { get; set; }
+        public string? MedicalInformation { get; set; }
+
+        public int? ClassId { get; set; }
+    }
+}
