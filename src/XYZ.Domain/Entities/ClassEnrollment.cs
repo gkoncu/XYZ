10d7aaca2a82e0fe6ed3e732d984719678be6a4a
gkoncu <gkoncu0@gmail.com>
2025-11-16 21:30:48 +0300

    Create ClassEnrollment.cs

---
+++ src/XYZ.Domain/Entities/ClassEnrollment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XYZ.Domain.Common;
+
+namespace XYZ.Domain.Entities
+{
+    public class ClassEnrollment : BaseEntity
+    {
+        public int StudentId { get; set; }
+        public int ClassId { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly? EndDate { get; set; }
+
+        public Student Student { get; set; } = null!;
+        public Class Class { get; set; } = null!;
+    }
+}
