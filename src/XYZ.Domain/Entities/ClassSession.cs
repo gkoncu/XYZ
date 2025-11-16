fb86051c1f4167a1e1d5b677522c22cb3e85f367
gkoncu <gkoncu0@gmail.com>
2025-11-16 21:21:32 +0300

    Create ClassSession

---
+++ src/XYZ.Domain/Entities/ClassSession.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XYZ.Domain.Common;
+using XYZ.Domain.Enums;
+
+namespace XYZ.Domain.Entities
+{
+    public class ClassSession : BaseEntity
+    {
+        public int ClassId { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public string? Location { get; set; }
+        public SessionStatus Status { get; set; } = SessionStatus.Planned;
+        public string? CoachNote { get; set; }
+        public Class Class { get; set; } = null!;
+        public ICollection<Attendance> Attendances { get; set; } = new List<Attendance>();
+    }
+}
---
+++ src/XYZ.Domain/Enums/SessionStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Domain.Enums
+{
+    public enum SessionStatus
+    {
+        Planned = 0,
+        Completed = 1,
+        Cancelled = 2
+    }
+}
