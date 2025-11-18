302a083e32b5dddef979e6edd3051053164937ff
gkoncu <gkoncu0@gmail.com>
2025-11-18 22:44:09 +0300

    Create Document Queries

---
+++ src/XYZ.Application/Features/Documents/Queries/GetStudentDocuments/GetStudentDocumentsQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Application.Features.Documents.Queries.GetStudentDocuments
+{
+    public class GetStudentDocumentsQueryValidator
+        : AbstractValidator<GetStudentDocumentsQuery>
+    {
+        public GetStudentDocumentsQueryValidator()
+        {
+            RuleFor(x => x.StudentId).GreaterThan(0);
+        }
+    }
+}
