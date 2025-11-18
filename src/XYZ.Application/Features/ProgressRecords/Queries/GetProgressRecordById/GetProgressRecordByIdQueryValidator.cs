a2057662e2898f2a713813e1d7e073b74e5f4f47
gkoncu <gkoncu0@gmail.com>
2025-11-18 20:52:20 +0300

    Create ProgressRecord Queries

---
+++ src/XYZ.Application/Features/ProgressRecords/Queries/GetProgressRecordById/GetProgressRecordByIdQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Application.Features.ProgressRecords.Queries.GetProgressRecordById
+{
+    public class GetProgressRecordByIdQueryValidator
+        : AbstractValidator<GetProgressRecordByIdQuery>
+    {
+        public GetProgressRecordByIdQueryValidator()
+        {
+            RuleFor(x => x.Id).GreaterThan(0);
+        }
+    }
+}
---
+++ src/XYZ.Application/Features/ProgressRecords/Queries/GetStudentProgressRecords/GetStudentProgressRecordsQueryValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Application.Features.ProgressRecords.Queries.GetStudentProgressRecords
+{
+    public class GetStudentProgressRecordsQueryValidator
+        : AbstractValidator<GetStudentProgressRecordsQuery>
+    {
+        public GetStudentProgressRecordsQueryValidator()
+        {
+            RuleFor(x => x.StudentId).GreaterThan(0);
+        }
+    }
+}
