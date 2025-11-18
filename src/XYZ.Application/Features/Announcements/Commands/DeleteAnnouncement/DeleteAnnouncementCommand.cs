bca5ffb1f158df9178a72f9b3358ace87dbec16b
gkoncu <gkoncu0@gmail.com>
2025-11-18 18:56:19 +0300

    Create DeleteAnnouncement

---
+++ src/XYZ.Application/Features/Announcements/Commands/DeleteAnnouncement/DeleteAnnouncementCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Application.Features.Announcements.Commands.DeleteAnnouncement
+{
+    public class DeleteAnnouncementCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+    }
+}
--- src/XYZ.Application/Features/Announcements/Commands/UpdateAnnouncement/UpdateAnnouncementCommand.cs
+++ src/XYZ.Application/Features/Announcements/Commands/UpdateAnnouncement/UpdateAnnouncementCommand.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XYZ.Domain.Enums;
 
 namespace XYZ.Application.Features.Announcements.Commands.UpdateAnnouncement
 {
-    internal class UpdateAnnouncementCommand
+    public class UpdateAnnouncementCommand : IRequest<int>
     {
+        public int Id { get; set; }
+
+        public int? ClassId { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime PublishDate { get; set; }
+
+        public DateTime? ExpiryDate { get; set; }
+
+        public AnnouncementType Type { get; set; } = AnnouncementType.General;
+
+        public bool? IsActive { get; set; }
     }
 }
