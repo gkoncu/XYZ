44dcf5cae99ec396eb37eb88ddd4da83c3368acd
gkoncu <gkoncu0@gmail.com>
2025-11-23 20:03:16 +0300

    Update UpdateSessionAttendanceCommand.cs

--- src/XYZ.Application/Features/Attendances/Commands/UpdateSessionAttendance/UpdateSessionAttendanceCommand.cs
+++ src/XYZ.Application/Features/Attendances/Commands/UpdateSessionAttendance/UpdateSessionAttendanceCommand.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using MediatR;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using XYZ.Domain.Enums;
 
 namespace XYZ.Application.Features.Attendances.Commands.UpdateSessionAttendance
 {
-    internal class UpdateSessionAttendanceCommand
+    public sealed class UpdateSessionAttendanceCommand : IRequest<int>
     {
+        public int SessionId { get; set; }
+
+        public IList<UpdateSessionAttendanceItem> Items { get; set; }
+            = new List<UpdateSessionAttendanceItem>();
+    }
+
+    public sealed class UpdateSessionAttendanceItem
+    {
+        public int AttendanceId { get; set; }
+
+        public AttendanceStatus Status { get; set; }
+        public string? Note { get; set; }
+        public int? Score { get; set; }
+        public string? CoachComment { get; set; }
     }
 }
