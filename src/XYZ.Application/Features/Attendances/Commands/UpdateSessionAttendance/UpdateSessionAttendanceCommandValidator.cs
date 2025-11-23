4901b0dbed1b2d4fa45d2729e889073f4bbc210c
gkoncu <gkoncu0@gmail.com>
2025-11-23 20:02:55 +0300

    Create Commands UpdateSessionAttendance

---
+++ src/XYZ.Application/Features/Attendances/Commands/UpdateSessionAttendance/UpdateSessionAttendanceCommandValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace XYZ.Application.Features.Attendances.Commands.UpdateSessionAttendance
+{
+    public class UpdateSessionAttendanceCommandValidator
+        : AbstractValidator<UpdateSessionAttendanceCommand>
+    {
+        public UpdateSessionAttendanceCommandValidator()
+        {
+            RuleFor(x => x.SessionId)
+                .GreaterThan(0);
+
+            RuleFor(x => x.Items)
+                .NotNull()
+                .WithMessage("Attendance listesi boş olamaz.");
+
+            RuleForEach(x => x.Items).ChildRules(item =>
+            {
+                item.RuleFor(i => i.AttendanceId)
+                    .GreaterThan(0);
+
+                item.RuleFor(i => i.Status)
+                    .IsInEnum();
+
+                item.RuleFor(i => i.Score)
+                    .InclusiveBetween(0, 100)
+                    .When(i => i.Score.HasValue);
+            });
+        }
+    }
+}
