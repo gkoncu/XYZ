8f6ed2476f9d16753d236d059adad6ffca1273c7
gkoncu <gkoncu0@gmail.com>
2025-11-23 22:39:46 +0300

    Create Attendance Web Models

---
+++ src/XYZ.Web/Models/Attendance/SessionAttendanceDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace XYZ.Web.Models.Attendance
+{
+    public class SessionAttendanceDto
+    {
+        public int SessionId { get; set; }
+        public int ClassId { get; set; }
+        public string ClassName { get; set; } = string.Empty;
+
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string? Location { get; set; }
+        public int Status { get; set; }
+
+        public IList<SessionAttendanceStudentDto> Students { get; set; }
+            = new List<SessionAttendanceStudentDto>();
+    }
+
+    public class SessionAttendanceStudentDto
+    {
+        public int AttendanceId { get; set; }
+        public int StudentId { get; set; }
+
+        public string FullName { get; set; } = string.Empty;
+
+        public int Status { get; set; }
+        public string? Note { get; set; }
+        public int? Score { get; set; }
+        public string? CoachComment { get; set; }
+    }
+}
