5eea1d05e6ecb53a702dd0a40fd029b1c6b4d083
gkoncu <gkoncu0@gmail.com>
2025-11-25 18:40:55 +0300

    Create IExcelExporter.cs

---
+++ src/XYZ.Application/Common/Interfaces/IExcelExporter.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace XYZ.Application.Common.Interfaces
+{
+    public interface IExcelExporter
+    {
+        byte[] Export<T>(IList<T> rows, string sheetName);
+    }
+}
