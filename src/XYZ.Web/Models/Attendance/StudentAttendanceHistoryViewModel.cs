43a7246c7cc2f7505e0950be34e970ca32f897c5
gkoncu <gkoncu0@gmail.com>
2025-11-25 18:29:51 +0300

    Create Attendance Report

---
+++ src/XYZ.Web/Models/Attendance/StudentAttendanceHistoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using XYZ.Application.Features.Attendances.Queries.GetStudentAttendanceHistory;
+
+namespace XYZ.Web.Models.Attendance
+{
+    public class StudentAttendanceHistoryViewModel
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; } = string.Empty;
+
+        public DateOnly From { get; set; }
+        public DateOnly To { get; set; }
+
+        public IList<StudentAttendanceHistoryItemDto> Items { get; set; }
+            = new List<StudentAttendanceHistoryItemDto>();
+    }
+}
