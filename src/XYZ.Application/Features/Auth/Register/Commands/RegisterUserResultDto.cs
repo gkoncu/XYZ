18fa5f5a4fc71dd1e76668f439682b757c6a2a9d
gkoncu <gkoncu0@gmail.com>
2025-11-26 21:40:35 +0300

    Create RegisterUser Command

---
+++ src/XYZ.Application/Features/Auth/Register/Commands/RegisterUserResultDto.cs
@@ -0,0 +1,12 @@
+﻿namespace XYZ.Application.Features.Auth.Register.Commands
+{
+    public sealed class RegisterUserResultDto
+    {
+        public string UserId { get; init; } = string.Empty;
+        public string Email { get; init; } = string.Empty;
+        public string FullName { get; init; } = string.Empty;
+        public string Role { get; init; } = string.Empty;
+        public int TenantId { get; init; }
+        public string TempPassword { get; init; } = string.Empty;
+    }
+}
