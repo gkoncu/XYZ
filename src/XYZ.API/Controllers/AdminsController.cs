edb231f7d6f70e095adf70ce9bd18d673223f430
gkoncu <gkoncu0@gmail.com>
2025-11-27 20:55:36 +0300

    Update AdminsController.cs

--- src/XYZ.API/Controllers/AdminsController.cs
+++ src/XYZ.API/Controllers/AdminsController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading;
 using System.Threading.Tasks;
 using XYZ.Application.Common.Models;
+using XYZ.Application.Features.Admins.Commands.CreateAdmin;
+using XYZ.Application.Features.Admins.Commands.DeleteAdmin;
 using XYZ.Application.Features.Admins.Commands.UpdateAdmin;
 using XYZ.Application.Features.Admins.Queries.GetAdminById;
 using XYZ.Application.Features.Admins.Queries.GetAllAdmins;
@@ -90,5 +92,74 @@ namespace XYZ.API.Controllers
             bool CanManageUsers,
             bool CanManageFinance,
             bool CanManageSettings);
+
+        [HttpPost]
+        [ProducesResponseType(typeof(int), StatusCodes.Status201Created)]
+        public async Task<ActionResult<int>> Create(
+            [FromBody] CreateAdminRequest request,
+            CancellationToken cancellationToken)
+        {
+            var command = new CreateAdminCommand
+            {
+                UserId = request.UserId,
+                TenantId = request.TenantId,
+                IdentityNumber = request.IdentityNumber,
+                CanManageUsers = request.CanManageUsers,
+                CanManageFinance = request.CanManageFinance,
+                CanManageSettings = request.CanManageSettings
+            };
+
+            try
+            {
+                var id = await _mediator.Send(command, cancellationToken);
+                return CreatedAtAction(nameof(GetById), new { id }, id);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Forbid();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        public sealed record CreateAdminRequest(
+            string UserId,
+            int? TenantId,
+            string IdentityNumber,
+            bool CanManageUsers,
+            bool CanManageFinance,
+            bool CanManageSettings);
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<ActionResult<int>> Delete(
+            int id,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var deletedId = await _mediator.Send(
+                    new DeleteAdminCommand { AdminId = id },
+                    cancellationToken);
+
+                return Ok(deletedId);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Forbid();
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+        }
+
     }
 }
