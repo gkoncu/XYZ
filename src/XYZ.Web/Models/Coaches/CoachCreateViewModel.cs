d34c58c9c31d27ed2a562a5e7d41580d3e1042c5
gkoncu <gkoncu0@gmail.com>
2025-11-27 12:38:21 +0300

    Create Coach Models

---
+++ src/XYZ.Web/Models/Coaches/CoachCreateViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XYZ.Web.Models.Coaches
+{
+    public class CoachCreateViewModel
+    {
+        [Required, StringLength(100)]
+        [Display(Name = "Ad")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        [Display(Name = "Soyad")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        [Display(Name = "E-posta")]
+        public string Email { get; set; } = string.Empty;
+
+        [Phone]
+        [Display(Name = "Telefon")]
+        public string? PhoneNumber { get; set; }
+
+        [Display(Name = "Branş")]
+        public string? Branch { get; set; }
+
+        [Display(Name = "Hakkında / Özgeçmiş")]
+        public string? Bio { get; set; }
+
+        [Display(Name = "Notlar")]
+        public string? Notes { get; set; }
+    }
+}
---
+++ src/XYZ.Web/Models/Coaches/CoachEditViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XYZ.Web.Models.Coaches
+{
+    public class CoachEditViewModel
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        [Display(Name = "Ad")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required, StringLength(100)]
+        [Display(Name = "Soyad")]
+        public string LastName { get; set; } = string.Empty;
+
+        [EmailAddress]
+        [Display(Name = "E-posta")]
+        public string? Email { get; set; }
+
+        [Phone]
+        [Display(Name = "Telefon")]
+        public string? PhoneNumber { get; set; }
+
+        [Display(Name = "Branş")]
+        public string? Branch { get; set; }
+
+        [Display(Name = "Hakkında / Özgeçmiş")]
+        public string? Bio { get; set; }
+
+        [Display(Name = "Notlar")]
+        public string? Notes { get; set; }
+
+        [Display(Name = "Aktif mi?")]
+        public bool IsActive { get; set; }
+    }
+}
