000a6760f5edef9fe8a113e5e794db25b4866065
gkoncu <gkoncu0@gmail.com>
2026-01-22 20:41:57 +0300

    Create Classes Models

---
+++ src/XYZ.Web/Models/Classes/ClassDetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using XYZ.Application.Features.Classes.Queries.GetAllClasses;
+
+namespace XYZ.Web.Models.Classes
+{
+    public sealed class ClassDetailsViewModel
+    {
+        public ClassDetailDto Class { get; init; } = default!;
+        public List<SelectListItem> AvailableStudents { get; init; } = new();
+        public List<SelectListItem> AvailableCoaches { get; init; } = new();
+    }
+}
