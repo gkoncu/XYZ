f4d98bdf0a5d94358b7bc54470dcae59f09c85dd
gkoncu <gkoncu0@gmail.com>
2026-01-23 11:32:50 +0300

    Create ProgressRecords Models

---
+++ src/XYZ.Web/Models/ProgressRecords/StudentProgressRecordsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using XYZ.Application.Features.ProgressRecords.Queries.GetStudentProgressRecords;
+
+namespace XYZ.Web.Models.ProgressRecords
+{
+    public sealed class StudentProgressRecordsViewModel
+    {
+        public int StudentId { get; set; }
+        public string StudentFullName { get; set; } = string.Empty;
+
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+
+        public List<ProgressRecordListItemDto> Items { get; set; } = new();
+
+        public bool CanWrite { get; set; }
+    }
+}
