b4dbdef2558f3557816631459ecd196329c6ccd5
gkoncu <gkoncu0@gmail.com>
2026-01-24 16:51:18 +0300

    Create GetUserDocuments Query

---
+++ src/XYZ.Application/Features/Documents/Queries/GetUserDocuments/GetUserDocumentsQuery.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using XYZ.Domain.Enums;
+
+namespace XYZ.Application.Features.Documents.Queries.GetUserDocuments
+{
+    public class GetUserDocumentsQuery : IRequest<IList<DocumentListItemDto>>
+    {
+        public DocumentTarget Target { get; set; }
+        public int OwnerId { get; set; }
+        public int? DocumentDefinitionId { get; set; }
+    }
+}
---
+++ src/XYZ.Application/Features/Documents/Queries/GetUserDocuments/GetUserDocumentsQueryHandler.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using XYZ.Application.Common.Interfaces;
+using XYZ.Domain.Enums;
+
+namespace XYZ.Application.Features.Documents.Queries.GetUserDocuments
+{
+    public class GetUserDocumentsQueryHandler : IRequestHandler<GetUserDocumentsQuery, IList<DocumentListItemDto>>
+    {
+        private readonly IDataScopeService _dataScope;
+
+        public GetUserDocumentsQueryHandler(IDataScopeService dataScope)
+        {
+            _dataScope = dataScope;
+        }
+
+        public async Task<IList<DocumentListItemDto>> Handle(GetUserDocumentsQuery request, CancellationToken cancellationToken)
+        {
+            var q = _dataScope.Documents()
+                .Include(d => d.DocumentDefinition)
+                .AsQueryable();
+
+            q = request.Target switch
+            {
+                DocumentTarget.Student => q.Where(d => d.StudentId == request.OwnerId),
+                DocumentTarget.Coach => q.Where(d => d.CoachId == request.OwnerId),
+                _ => q.Where(_ => false)
+            };
+
+            if (request.DocumentDefinitionId.HasValue)
+                q = q.Where(d => d.DocumentDefinitionId == request.DocumentDefinitionId.Value);
+
+            return await q
+                .OrderByDescending(d => d.UploadDate)
+                .Select(d => new DocumentListItemDto
+                {
+                    Id = d.Id,
+                    Name = d.Name,
+                    Description = d.Description,
+                    UploadDate = d.UploadDate,
+                    UploadedBy = d.UploadedBy,
+                    DocumentDefinitionId = d.DocumentDefinitionId,
+                    DocumentDefinitionName = d.DocumentDefinition.Name,
+                    IsRequired = d.DocumentDefinition.IsRequired,
+                    Target = d.DocumentDefinition.Target
+                })
+                .ToListAsync(cancellationToken);
+        }
+    }
+}
