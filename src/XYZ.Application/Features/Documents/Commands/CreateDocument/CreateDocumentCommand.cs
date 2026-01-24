42fb561c23beec7c63e049c637a4b80e55493e07
gkoncu <gkoncu0@gmail.com>
2026-01-24 16:47:34 +0300

    Update Document Commands

--- src/XYZ.Application/Features/Documents/Commands/CreateDocument/CreateDocumentCommand.cs
+++ src/XYZ.Application/Features/Documents/Commands/CreateDocument/CreateDocumentCommand.cs
@@ -1,20 +1,16 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using XYZ.Domain.Enums;
 
 namespace XYZ.Application.Features.Documents.Commands.CreateDocument
 {
     public class CreateDocumentCommand : IRequest<int>
     {
-        public int StudentId { get; set; }
+        public int DocumentDefinitionId { get; set; }
+
+        public int? StudentId { get; set; }
+        public int? CoachId { get; set; }
 
         public string Name { get; set; } = string.Empty;
         public string FilePath { get; set; } = string.Empty;
         public string? Description { get; set; }
-        public DocumentType Type { get; set; } = DocumentType.Other;
     }
 }
