ee8f971a8e4e4730c106423c0f8e7b5071854e55
gkoncu <gkoncu0@gmail.com>
2026-01-24 21:24:25 +0300

    Create Document Models

---
+++ src/XYZ.Web/Models/Documents/DocumentOwnerIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using XYZ.Application.Features.Documents.Queries.DocumentStatus;
+
+namespace XYZ.Web.Models.Documents
+{
+    public class DocumentOwnerIndexViewModel<TItem>
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public bool OnlyIncomplete { get; set; } = true;
+        public string? SearchTerm { get; set; }
+
+        public IList<TItem> Items { get; set; } = new List<TItem>();
+    }
+}
