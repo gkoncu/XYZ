29598b69721c52d09951d163408a3c268736b678
gkoncu <gkoncu0@gmail.com>
2026-01-29 22:36:40 +0300

    Create New Extensions

---
+++ src/XYZ.Web/Extensions/BloodTypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using XYZ.Domain.Enums;
+
+namespace XYZ.Web.Extensions
+{
+    public static class BloodTypeExtensions
+    {
+        public static string ToDisplayName(this BloodType bloodType)
+        {
+            return bloodType switch
+            {
+                BloodType.A_Positive => "A+",
+                BloodType.A_Negative => "A-",
+                BloodType.B_Positive => "B+",
+                BloodType.B_Negative => "B-",
+                BloodType.AB_Positive => "AB+",
+                BloodType.AB_Negative => "AB-",
+                BloodType.O_Positive => "O+",
+                BloodType.O_Negative => "O-",
+                _ => "Bilinmiyor"
+            };
+        }
+    }
+}
---
+++ src/XYZ.Web/Extensions/GenderExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+using XYZ.Domain.Enums;
+
+namespace XYZ.Web.Extensions
+{
+    public static class GenderExtensions
+    {
+        public static string ToDisplayName(this Gender gender)
+        {
+            return gender switch
+            {
+                Gender.Male => "Erkek",
+                Gender.Female => "Kadın",
+                Gender.PreferNotToSay => "Belirtmek İstemiyorum",
+                _ => "Bilinmiyor"
+
+            };
+        }
+    }
+}
