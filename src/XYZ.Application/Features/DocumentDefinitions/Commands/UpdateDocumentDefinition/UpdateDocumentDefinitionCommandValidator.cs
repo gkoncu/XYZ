5af2d6fadff1d137d5efc5cc80ffee507cb970b5
gkoncu <gkoncu0@gmail.com>
2026-01-29 22:43:19 +0300

    Update DocumentDefinitions

--- src/XYZ.Application/Features/DocumentDefinitions/Commands/UpdateDocumentDefinition/UpdateDocumentDefinitionCommandValidator.cs
+++ src/XYZ.Application/Features/DocumentDefinitions/Commands/UpdateDocumentDefinition/UpdateDocumentDefinitionCommandValidator.cs
@@ -6,9 +6,21 @@ namespace XYZ.Application.Features.DocumentDefinitions.Commands.UpdateDocumentDe
     {
         public UpdateDocumentDefinitionCommandValidator()
         {
-            RuleFor(x => x.Id).GreaterThan(0);
-            RuleFor(x => x.Name).NotEmpty().MaximumLength(200);
-            RuleFor(x => x.SortOrder).GreaterThanOrEqualTo(0);
+            RuleFor(x => x.Id)
+                .GreaterThan(0);
+
+            RuleFor(x => x.Target)
+                .IsInEnum();
+
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .MaximumLength(80);
+
+            RuleFor(x => x.SortOrder)
+                .InclusiveBetween(0, 10000);
+
+            RuleFor(x => x.IsActive)
+                .NotNull();
         }
     }
 }
--- src/XYZ.Web/Models/DocumentDefinitions/DocumentDefinitionEditViewModel.cs
+++ src/XYZ.Web/Models/DocumentDefinitions/DocumentDefinitionEditViewModel.cs
@@ -1,17 +1,24 @@
-﻿namespace XYZ.Web.Models.DocumentDefinitions
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace XYZ.Web.Models.DocumentDefinitions
 {
     public class DocumentDefinitionEditViewModel
     {
+        [Range(0, int.MaxValue, ErrorMessage = "Geçersiz kayıt.")]
         public int Id { get; set; }
 
+        [Range(1, 2, ErrorMessage = "Geçersiz hedef tipi.")]
         public int Target { get; set; }
 
+        [Required(ErrorMessage = "Belge adı zorunludur.")]
+        [StringLength(80, ErrorMessage = "Belge adı en fazla 80 karakter olabilir.")]
         public string Name { get; set; } = string.Empty;
 
         public bool IsRequired { get; set; }
 
         public bool IsActive { get; set; } = true;
 
+        [Range(0, 10000, ErrorMessage = "Sıra değeri geçersiz.")]
         public int SortOrder { get; set; } = 100;
     }
 }
