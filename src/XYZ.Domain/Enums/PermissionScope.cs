b7c88d63891fd65ceea0f0b4e8f8c3da6d2271e0
gkoncu <gkoncu0@gmail.com>
2026-02-13 20:57:50 +0300

    Update Entities with Permission

---
+++ src/XYZ.Domain/Enums/PermissionScope.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XYZ.Domain.Enums
+{
+    internal class PermissionScope
+    {
+    }
+}
