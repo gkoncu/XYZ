b5a4b77a83a4ce50fc5539f5eed764bcdb11cf77
gkoncu <gkoncu0@gmail.com>
2026-02-15 14:55:40 +0300

    Update Payments Queries

--- src/XYZ.Application/Features/Payments/Queries/GetPayments/GetPaymentsQuery.cs
+++ src/XYZ.Application/Features/Payments/Queries/GetPayments/GetPaymentsQuery.cs
@@ -1,22 +1,20 @@
 ﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using XYZ.Application.Common.Interfaces;
 using XYZ.Application.Common.Models;
+using XYZ.Domain.Constants;
 using XYZ.Domain.Enums;
 
-namespace XYZ.Application.Features.Payments.Queries.GetPayments
+namespace XYZ.Application.Features.Payments.Queries.GetPayments;
+
+public sealed class GetPaymentsQuery : IRequest<PaginationResult<PaymentListItemDto>>, IRequirePermission
 {
-    public class GetPaymentsQuery
-        : IRequest<PaginationResult<PaymentListItemDto>>
-    {
-        public int? StudentId { get; set; }
-        public PaymentStatus? Status { get; set; }
-        public DateOnly? FromDueDate { get; set; }
-        public DateOnly? ToDueDate { get; set; }
-        public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 20;
-    }
+    public string PermissionKey => PermissionNames.Students.PaymentsRead;
+    public PermissionScope? MinimumScope => PermissionScope.Self;
+
+    public int? StudentId { get; set; }
+    public PaymentStatus? Status { get; set; }
+    public DateOnly? FromDueDate { get; set; }
+    public DateOnly? ToDueDate { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 20;
 }
